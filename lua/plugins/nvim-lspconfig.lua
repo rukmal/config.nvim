6664a41d5a836f7d2a50c242e7d249355bcae65f
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2025-05-06 20:55:06 -0400

    Update lazy packages and lspconfig (need to switch to nvim native version

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,7 +2,7 @@ return {
     "neovim/nvim-lspconfig",
     event = "LazyFile",
     dependencies = {
-        { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
+        -- { "folke/neoconf.nvim", cmd = "Neoconf", config = false, dependencies = { "nvim-lspconfig" } },
         "mason.nvim",
         "williamboman/mason-lspconfig.nvim",
         {
@@ -17,4 +17,6 @@ return {
     opts = {
         inlay_hints = { enabled = true },
     },
+    config = function()
+    end,
 }
