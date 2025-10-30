5b40644c3e433684dad28722dca20b7728ab4289
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2025-10-31 04:05:26 +0530

    Change theme installation and update trouble settings

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,8 +2,7 @@ return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        focus = false; -- focus on the trouble window when opened
+        follow = true; -- follow the item under the cursor
     },
 }
