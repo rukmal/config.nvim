c48e582034ec058827dc3c0943e5f7c13b86bc95
Rukmal Weerawarana <rukmal.weerawarana@gmail.com>
2026-02-04 20:45:28 +0530

    Install diffview to look at diffs between last commit and HEAD - useful when using AI CLIs

---
+++ lua/plugins/diffview.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory", "DiffviewClose" },
+    keys = {
+      { "<leader>cD", "<cmd>DiffviewOpen<cr>", desc = "Diffview" },
+    },
+  },
+}
